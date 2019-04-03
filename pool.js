70c0b9ec7d481b3687ac2e7a0566d49d97c002d1
lingmx <529247970@qq.com>
2019-04-03 20:01:23 +0800

    修改package.json和pool.js

--- pool.js
+++ pool.js
@@ -2,12 +2,12 @@
 const mysql=require('mysql');
 //建立数据库连接
 var pool=mysql.createPool({
-	host:'127.0.0.1',
-	port:'3306',
-	user:'root',
-	password:'',
-	database:'nintendo',
-	connectionLimit:20,
+	host     : process.env.MYSQL_HOST,
+    port     : process.env.MYSQL_PORT,
+    user     : process.env.ACCESSKEY,
+    password : process.env.SECRETKEY,
+    database : 'app_' + process.env.APPNAME,
+	connectionLimit:3,
 	multipleStatements: true  //允许执行多条SQL语句
 });
 //导出连接池对象
